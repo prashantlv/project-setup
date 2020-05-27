414700e5d2f2ec7513135e11450b401f0f8e7d27
Prashant Shukla <shukla.prashant689@gmail.com>
2020-05-27 22:13:59 +0530

    Update pro_init.py

--- pro_init.py
+++ pro_init.py
@@ -2,13 +2,11 @@
     import os
     from github import Github
     path = "/path_to_project_directory"
-    
     def create():
        folder_name = str(sys.argv[1])
        os.makedirs(path+folder_name)
        user = Github(username,password).get_user()
        repo = user.create_repo(folder_name)
        print(f"New Repository '{folder_name}' created successfully.")
-    
     if __name__ == "__main__":
        create()

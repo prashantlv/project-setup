6cf26e43ade644cac49e134eefc7b9c20b7c74e0
Prashant Shukla <shukla.prashant689@gmail.com>
2020-05-28 20:10:30 +0530

    now open Visual Studio code

--- run_me.sh
+++ run_me.sh
@@ -13,5 +13,5 @@
        git remote add origin   		https://github.com/<your_git_username>/$1.git
        git push -u origin master
        
-       code . #opens up new directory in vs code
+       code . #opens VScode in same directory
     }
